59f59a2c698299b553d83b0d263b3c6970fdec61
Adonis Thaci <adonis.t.at@goolemail.com>
2015-01-08 12:35:07 +0100

    push everthing

---
+++ Arbeitsauftrag-Umweltmessung/Arbeitsauftrag-Umweltmessung/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbeitsauftrag_Umweltmessung
+{
+    class Umweltmessung
+    {
+        static int[,,] messungen = new int[365,1,1];
+
+        static void Main(string[] args)
+        {
+
+            umweltmessungMenu();
+            Console.ReadLine();
+
+        }
+
+        static void umweltmessungMenu()
+        {
+            Umweltmessung um = new Umweltmessung();
+            
+
+            Console.WriteLine("Willkommen bei dem Umweltmesser");
+            Console.WriteLine("Bitte Wählen sie zwischen folgenden Punkten");
+            Console.WriteLine(" _______________________________________________");
+            Console.WriteLine("|(1) Messwerte eingeben                         |");
+            Console.WriteLine("|(2) Mittelwert Abfragen                        |");
+            Console.WriteLine("|(3) Messwerte eines Tages verändern            |");
+            Console.WriteLine("|(4) Exportieren der daten                      |");
+            Console.WriteLine("|_______________________________________________|");
+            int eingabe = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine();
+
+            switch (eingabe)
+            {
+                case 1:
+                    um.messwertEingebe();
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        public void messwertEingebe()
+        {
+            Console.WriteLine("Messwerte eingeben");
+            int arrayLenght = messungen.Length;
+            Console.WriteLine(arrayLenght);
+
+        }
+    }
+}
